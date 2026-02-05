153b1b95fb790ac45ee065a72dc578576b2ece61
Carlos Henrique Carneiro Costa <carloshcarneiroc@gmail.com>
2026-02-05 18:11:17 -0300

    adicionando projetos

---
+++ 1_🏠_home.py
@@ -0,0 +1,102 @@
+# Importando bibliotecas
+import streamlit as st
+import webbrowser
+import pandas as pd
+from datetime import datetime 
+
+# Lendo o arquivo e filtrando
+if "data" not in st.session_state:
+    df_data = pd.read_csv("datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
+    df_data = df_data[df_data["Contract Valid Until"] >= 2022]
+    df_data = df_data[df_data["Value(£)"] > 0]
+    df_data = df_data.sort_values(by="Overall", ascending=False)
+    st.session_state["data"] = df_data
+
+# Titulo
+st.markdown("# FIFA23 OFFICIAL DATASET! ⚽️")
+# recado na sidebar
+st.sidebar.markdown("Desenvolvido por [Asimov Academy](https://asimov.academy)")
+
+# botão com link pro site
+btn = st.button("Acesse os dados no Kaggle")
+if btn:
+    webbrowser.open_new_tab("https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
+
+# texto
+st.markdown(
+    """
+    O conjunto de dados
+    de jogadores de futebol de 2017 a 2023 fornece informações 
+    abrangentes sobre jogadores de futebol profissionais.
+    O conjunto de dados contém uma ampla gama de atributos, incluindo dados demográficos 
+    do jogador, características físicas, estatísticas de jogo, detalhes do contrato e 
+    afiliações de clubes. 
+    
+    Com **mais de 17.000 registros**, este conjunto de dados oferece um recurso valioso para 
+    analistas de futebol, pesquisadores e entusiastas interessados em explorar vários 
+    aspectos do mundo do futebol, pois permite estudar atributos de jogadores, métricas de 
+    desempenho, avaliação de mercado, análise de clubes, posicionamento de jogadores e 
+    desenvolvimento do jogador ao longo do tempo.
+"""
+)
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+st.text_input("*Your name*", key="name")
+
+# You can access the value at any point with:
+st.write("Hello", st.session_state.name)
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
+
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
+
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
+
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
+
+
+import time
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
---
+++ pages/3_⚽️_teams.py
@@ -0,0 +1,55 @@
+# Importando bibliotecas
+import streamlit as st
+import requests
+import base64
+
+# o mesmo feito em players
+@st.cache_data
+def load_image_64(url):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    data = requests.get(url, headers=headers).content
+    return "data:image/png;base64," + base64.b64encode(data).decode()
+
+def preprocess_row(url):
+    if isinstance(url, str) and url.startswith("http"):
+        return load_image_64(url)
+    return url
+
+st.set_page_config(
+    page_title="Players",
+    page_icon="🏃🏼",
+    layout="wide"
+)
+
+df_data = st.session_state["data"]
+
+clubes = df_data["Club"].value_counts().index
+club = st.sidebar.selectbox("Clube", clubes)
+# o mesmo até aqui
+
+# pre processando o df exibido
+df_filtered = df_data[(df_data["Club"] == club)].set_index("Name")
+df_filtered["Photo"] = df_filtered["Photo"].apply(preprocess_row)
+df_filtered["Flag"] = df_filtered["Flag"].apply(preprocess_row)
+df_filtered["Club Logo"] = df_filtered["Club Logo"].apply(preprocess_row)
+
+# Exibindo escudo e nome
+st.image(df_filtered.iloc[0]["Club Logo"])
+st.markdown(f"## {club}")
+
+# Escolhendo infos a serem exibidas
+columns = ["Age", "Photo", "Flag", "Overall", 'Value(£)', 'Wage(£)', 'Joined', 
+           'Height(cm.)', 'Weight(lbs.)',
+           'Contract Valid Until', 'Release Clause(£)']
+
+# Exibindo df e configurando colunas desejadas
+st.dataframe(df_filtered[columns],
+             column_config={
+                 "Overall": st.column_config.ProgressColumn(
+                     "Overall", format="%d", min_value=0, max_value=100
+                 ),
+                 "Wage(£)": st.column_config.ProgressColumn("Weekly Wage", format="£%f", 
+                                                    min_value=0, max_value=df_filtered["Wage(£)"].max()),
+                "Photo": st.column_config.ImageColumn(),
+                "Flag": st.column_config.ImageColumn("Country"),
+             })
