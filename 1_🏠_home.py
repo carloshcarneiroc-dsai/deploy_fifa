e6a407b9f4201db7f171b71e56edb9def0e8f45e
Carlos Henrique Carneiro Costa <carloshcarneiroc@gmail.com>
2026-02-05 18:47:39 -0300

    Update 1_🏠_home.py

--- 1_🏠_home.py
+++ 1_🏠_home.py
@@ -19,9 +19,10 @@ st.markdown("# FIFA23 OFFICIAL DATASET! ⚽️")
 st.sidebar.markdown("Desenvolvido por [Asimov Academy](https://asimov.academy)")
 
 # botão com link pro site
-btn = st.button("Acesse os dados no Kaggle")
-if btn:
-    webbrowser.open_new_tab("https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
+btn = st.link_button(
+    "Acesse os dados no Kaggle",
+    "https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data"
+    )
 
 # texto
 st.markdown(
@@ -41,63 +42,63 @@ st.markdown(
 """
 )
 
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+#x = st.slider('x')  # 👈 this is a widget
+#st.write(x, 'squared is', x * x)
 
-st.text_input("*Your name*", key="name")
+#st.text_input("*Your name*", key="name")
 
 # You can access the value at any point with:
-st.write("Hello", st.session_state.name)
+#st.write("Hello", st.session_state.name)
 
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-    })
+#df = pd.DataFrame({
+#    'first column': [1, 2, 3, 4],
+#    'second column': [10, 20, 30, 40]
+#    })
 
-option = st.selectbox(
-    'Which number do you like best?',
-     df['first column'])
+#option = st.selectbox(
+#    'Which number do you like best?',
+#     df['first column'])
 
-'You selected: ', option
+#'You selected: ', option
 
 
 # Add a selectbox to the sidebar:
-add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
-)
+#add_selectbox = st.sidebar.selectbox(
+#    'How would you like to be contacted?',
+#    ('Email', 'Home phone', 'Mobile phone')
+#)
 
 # Add a slider to the sidebar:
-add_slider = st.sidebar.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0)
-)
+#add_slider = st.sidebar.slider(
+#    'Select a range of values',
+#    0.0, 100.0, (25.0, 75.0)
+#)
 
 
-left_column, right_column = st.columns(2)
+#left_column, right_column = st.columns(2)
 # You can use a column just like st.sidebar:
-left_column.button('Press me!')
+#left_column.button('Press me!')
 
 # Or even better, call Streamlit functions inside a "with" block:
-with right_column:
-    chosen = st.radio(
-        'Sorting hat',
-        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-    st.write(f"You are in {chosen} house!")
+#with right_column:
+#    chosen = st.radio(
+#        'Sorting hat',
+#        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+#    st.write(f"You are in {chosen} house!")
 
 
-import time
+#import time
 
-'Starting a long computation...'
+#'Starting a long computation...'
 
 # Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
+#latest_iteration = st.empty()
+#bar = st.progress(0)
 
-for i in range(100):
+#for i in range(100):
   # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
+#  latest_iteration.text(f'Iteration {i+1}')
+#  bar.progress(i + 1)
+#  time.sleep(0.1)
 
-'...and now we\'re done!'
+#'...and now we\'re done!'
